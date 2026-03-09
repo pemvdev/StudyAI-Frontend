8657cd428ce8e681c19c207ee2474cbbf30fd87b
Pedro Valim <dev.pedrovalim@gmail.com>
2026-03-09 00:44:55 -0300

    Arrumando modo noturno

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,14 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UserProfileComponent } from "../user-profile/user-profile.component";
 import { RouterModule } from "@angular/router";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [UserProfileComponent, RouterModule],
+  imports: [UserProfileComponent, RouterModule, CommonModule ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  isDark = false
+
+  ngOnInit() {
+
+    const savedTheme = localStorage.getItem("theme") || "light"
+
+    document.documentElement.setAttribute("data-bs-theme", savedTheme)
+
+    this.isDark = savedTheme === "dark"
+  }
+
+  toggleTheme() {
+  const html = document.documentElement;
+  if (html.getAttribute('data-bs-theme') === 'dark') {
+    html.setAttribute('data-bs-theme', 'light');
+  } else {
+    html.setAttribute('data-bs-theme', 'dark');
+  }
+  localStorage.setItem("theme", html.getAttribute('data-bs-theme') || "light")
+  this.isDark = html.getAttribute('data-bs-theme') === 'dark'
+}
 
 }
